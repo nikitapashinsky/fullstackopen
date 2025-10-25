6ee54165a0a2019ef2f97cfcc520d112f7543453
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-10-25 16:42:20 +0200

    courseinfo: move components out of App component 🤦

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,34 @@
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+
+  return <p>Number of exercises {total}</p>;
+};
+
 const App = () => {
   const course = {
     name: "Half Stack application development",
@@ -15,37 +46,6 @@ const App = () => {
         exercises: 14,
       },
     ],
-  };
-
-  const Header = (props) => {
-    return <h1>{props.course.name}</h1>;
-  };
-
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    );
-  };
-
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-      </div>
-    );
-  };
-
-  const Total = (props) => {
-    const total =
-      props.parts[0].exercises +
-      props.parts[1].exercises +
-      props.parts[2].exercises;
-
-    return <p>Number of exercises {total}</p>;
   };
 
   return (
