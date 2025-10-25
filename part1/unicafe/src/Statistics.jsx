5afbe7498352fbcc3e31485d509eb249188ffef7
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-10-25 19:37:08 +0200

    complete 1.9: unicafe step 4

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -10,31 +10,35 @@ const Statistics = ({
   return (
     <div className="flex flex-col gap-3">
       <h1 className="text-2xl font-medium">statistics</h1>
-      <div className="flex flex-col gap-3">
-        <div className="grid grid-cols-2 gap-1 text-sm">
-          <p>good</p>
-          <p>{good}</p>
+      {totalFeedbacks > 0 ? (
+        <div className="flex flex-col gap-3">
+          <div className="grid grid-cols-2 gap-1 text-sm">
+            <p>good</p>
+            <p>{good}</p>
 
-          <p>neutral</p>
-          <p>{neutral}</p>
+            <p>neutral</p>
+            <p>{neutral}</p>
 
-          <p>bad</p>
-          <p>{bad}</p>
+            <p>bad</p>
+            <p>{bad}</p>
 
-          <p>total</p>
-          <p>{totalFeedbacks}</p>
+            <p>total</p>
+            <p>{totalFeedbacks}</p>
+          </div>
+          <div className="grid grid-cols-2 gap-1 text-sm">
+            <p>total score</p>
+            <p>{totalScore}</p>
+
+            <p>average score</p>
+            <p>{average.toFixed(2)}</p>
+
+            <p>positive</p>
+            <p>{positive.toFixed(2)}%</p>
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-1 text-sm">
-          <p>total score</p>
-          <p>{totalScore}</p>
-
-          <p>average score</p>
-          <p>{average.toFixed(2)}</p>
-
-          <p>positive</p>
-          <p>{positive.toFixed(2)}%</p>
-        </div>
-      </div>
+      ) : (
+        <p className="text-sm">no feedbacks given yet</p>
+      )}
     </div>
   );
 };
