69a343542e785d1846f45711ec3454a2ad82335a
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-10-25 15:38:44 +0200

    Complete 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,19 +11,21 @@ const App = () => {
     return <h1>{props.course}</h1>;
   };
 
-  const Content = (props) => {
+  const Part = (props) => {
     return (
-      <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
-      </>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    );
+  };
+
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+      </div>
     );
   };
 
@@ -39,14 +41,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
+      <Content />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
