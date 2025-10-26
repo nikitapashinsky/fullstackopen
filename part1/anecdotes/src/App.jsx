208e9a3c42acac9a38af7936afc96637595e0038
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-10-26 10:04:27 +0100

    complete 1.13: anecdotes step 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -12,25 +12,40 @@ const App = () => {
     "The only way to go fast, is to go well.",
   ];
 
+  const arr = new Array(anecdotes.length).fill(0);
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(arr);
 
-  function handleClick() {
+  function handleNextClick() {
     const newSelected = Math.floor(Math.random() * anecdotes.length);
     setSelected(newSelected);
     console.log(newSelected);
   }
 
+  function handleVoteClick() {
+    const votesCopy = [...votes];
+    votesCopy[selected] += 1;
+    setVotes(votesCopy);
+  }
+
   return (
-    <div className="flex flex-col p-2 h-full">
-      <div className="flex-2 p-4">
-        <h1 className="text-3xl font-medium max-w-3xl">
-          {anecdotes[selected]}
-        </h1>
+    <div className="flex flex-col gap-2 p-2 h-full">
+      <div className="flex-2 p-2 sm:p-4">
+        <h1 className="text-2xl sm:text-3xl font-medium max-w-3xl">{anecdotes[selected]}</h1>
       </div>
-      <div className="flex-1">
+      <div className="px-2 sm:px-4 pb-4 text-2xl sm:text-3xl font-medium text-neutral-500">
+        {votes[selected]} votes
+      </div>
+      <div className="flex flex-col sm:flex-row flex-1 gap-2">
         <button
-          onClick={handleClick}
-          className="w-full h-full bg-neutral-100 rounded-md min-w-0 self-start hover:bg-neutral-200 active:scale-[0.985] transition"
+          onClick={handleVoteClick}
+          className="w-full h-full bg-neutral-100 text-2xl sm:text-3xl font-medium rounded-xl min-w-0 self-start hover:bg-neutral-200 active:scale-[0.985] transition"
+        >
+          vote
+        </button>
+        <button
+          onClick={handleNextClick}
+          className="w-full h-full bg-neutral-100 text-2xl sm:text-3xl font-medium rounded-xl min-w-0 self-start hover:bg-neutral-200 active:scale-[0.985] transition"
         >
           next anecdote
         </button>
