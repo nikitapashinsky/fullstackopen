49fdafb0c8c46081f85000a50f8a26d59a727b8c
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-11-08 16:43:09 +0100

    complete 2.7: phonebook step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 
 const App = () => {
+  console.clear();
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+
+  function addPerson(e) {
+    e.preventDefault();
+
+    if (!newName.trim()) {
+      setNewName("");
+      return;
+    }
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`A person with name ${newName} already exists.`);
+      return;
+    }
+
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  }
 
   return (
     <div>
       {/* <div id="debug">debug: {newName}</div>*/}
       <h2>Phonebook</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const newPersons = [...persons, { name: newName }];
-          setPersons(newPersons);
-        }}
-      >
+      <form onSubmit={addPerson}>
         <label htmlFor="name">Name:</label>
         <input
+          required
           id="name"
           type="text"
           value={newName}
