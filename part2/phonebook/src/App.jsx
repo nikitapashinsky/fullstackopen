2c594c02d46c82f479ae44e88f4051029cb832a0
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-11-08 17:33:25 +0100

    complete 2.9*: phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   console.clear();
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "+19257291478" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "+19365331007", id: 1 },
+    { name: "Ada Lovelace", phone: "+18772688722", id: 2 },
+    { name: "Dan Abramov", phone: "+13365917243", id: 3 },
+    { name: "Mary Poppendieck", phone: "+18433509567", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [filterString, setFilterString] = useState("");
 
   const phoneRegex = /\+?1?\s*\(?\d{3}\)?\s*\d{3}\s*\d{4}(?:\s?(?:x|ext)\.?\s?\d{1,5})?/;
 
@@ -31,14 +37,13 @@ const App = () => {
       return;
     }
 
-    setPersons([...persons, { name: newName, phone: newPhone }]);
+    setPersons([...persons, { name: newName, phone: newPhone, id: persons.at(-1).id + 1 }]);
     setNewName("");
     setNewPhone("");
   }
 
   return (
     <div>
-      {/* <div id="debug">debug: {newName}</div>*/}
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <label htmlFor="name">
@@ -68,13 +73,22 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <h2>People</h2>
+      <input
+        type="text"
+        placeholder="Filter by name"
+        value={filterString}
+        onChange={(e) => setFilterString(e.target.value.toLowerCase())}
+      />
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>
-            <span>{person.name}</span>
-            <span>{person.phone}</span>
-          </li>
-        ))}
+        {persons.map(
+          (person) =>
+            person.name.toLowerCase().includes(filterString) && (
+              <li key={person.id}>
+                <span>{person.name}</span>
+                <span>{person.phone}</span>
+              </li>
+            ),
+        )}
       </ul>
     </div>
   );
